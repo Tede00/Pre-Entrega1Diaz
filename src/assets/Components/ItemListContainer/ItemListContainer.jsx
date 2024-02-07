8b76ec3c0e12eabd159f3a511bc116bbeebe7690
Tomas Diaz <diaztomas6885@gmail.com>
2024-02-07 15:48:58 -0300

    ReactLoading

--- src/assets/Components/ItemListContainer/ItemListContainer.jsx
+++ src/assets/Components/ItemListContainer/ItemListContainer.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { pedirDatos, pedirDatosDeCategoria } from '../../../Data'
 import ItemList from '../ItemList/ItemList'
+import ReactLoading from 'react-loading';
 
 const ItemListContainer = () => {
     const [products, setProducts] = useState([])
@@ -24,7 +25,7 @@ const ItemListContainer = () => {
         <h1>PRODUCTOS</h1>
         {
             loading 
-            ? <p>CARGANDO... </p>
+            ? <div className='d-flex justify-content-center'><ReactLoading type={'bubbles'} color={'#AB34DB'} height={'20%'} width={'20%'} /></div>
             : <ItemList products={products} />
         }
     </>
